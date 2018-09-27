1357770da20c1754ce892e741e6a2472538b9394
Zach Stence <zachstence@gmail.com>
2018-09-27 16:17:46 -0500

    Add function to get filter buses on a specific route
    - get_buses_on_route() takes an int parameter (the DoubleMap route ID) and returns a list of dictionaries (similar to get_buses()) but only buses on the specified route (Fixes #7)

--- poll_api.py
+++ poll_api.py
@@ -1,12 +1,28 @@
 import requests
 import json
+import pprint
 
 
 def get_buses():
+    """
+    Polls the DoubleMap API.
+    :return: A list of dictionaries corresponding to the buses currently in service.
+    """
     r = requests.get('http://txstate.doublemap.com/map/v2/buses')
     buses = json.loads(r.text)
     return buses
 
 
+def get_buses_on_route(route):
+    """
+    Polls the DoubleMap API and filters the results to buses only on a certain route.
+    :param route: The DoubleMap integer route ID
+    :return: A list of dictionaries corresponding to the buses currently in service on the specified route.
+    """
+    buses = get_buses()
+    on_route = [bus for bus in buses if bus['route'] == route]
+    return on_route
+
+
 if __name__ == "__main__":
-    print(get_buses())
+    pprint.pprint(get_buses_on_route(639))
